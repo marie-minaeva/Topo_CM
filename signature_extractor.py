65b75756afb1cee5365cc940885f7c1e953a3428
petite_marie <minaeva.mav@gmail.com>
2020-12-02 13:37:48 +0300

    Updated a function to extract signatures from DE files

--- signature_extractor.py
+++ signature_extractor.py
@@ -22,11 +22,11 @@ def signature_extractor(file):
     # calculating up/down regulated genes
 
     if names[0] == "logFC" and names[2] == "PValue":
-        up_genes = stat[(stat["logFC"] >= 2) & (stat["PValue"] <= 1e-3)].index
-        down_genes = stat[(stat["logFC"] <= -2) & (stat["PValue"] <= 1e-3)].index
+        up_genes = stat[(stat["logFC"] >= 1.5) & (stat["PValue"] <= 1e-3)].index
+        down_genes = stat[(stat["logFC"] <= -1.5) & (stat["PValue"] <= 1e-3)].index
 
     if names[1] == "log2FoldChange" and names[5] == "padj":
-        up_genes = stat[(stat["log2FoldChange"] >= 2) & (stat["padj"] <= 1e-3)].index
-        down_genes = stat[(stat["log2FoldChange"] <= -2) & (stat["padj"] <= 1e-3)].index
+        up_genes = stat[(stat["log2FoldChange"] >= 1.5) & (stat["padj"] <= 1e-3)].index
+        down_genes = stat[(stat["log2FoldChange"] <= -1.5) & (stat["padj"] <= 1e-3)].index
 
     return up_genes, down_genes
