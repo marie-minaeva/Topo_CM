cec3d3c0ad5fd75ea5729f9cd2509918903aff0e
petite_marie <minaeva.mav@gmail.com>
2021-02-27 17:37:45 +0300

    Minor changes

--- cholm_test.py
+++ cholm_test.py
@@ -2,27 +2,22 @@ from scipy import stats
 import pandas as pd
 import numpy as np
 import seaborn as sns
-import matplotlib.pyplot as plt
 
+def kolm_test(cmap_db, pref):
+    cfm_db = pd.read_csv("~/Topo_Camp/Data/intersection_of_CFM_and_L1000FWD.csv")
+    dist = []
+    cids = []
+    cfm_db["CID"] = [str(cid) for cid in cfm_db["CID"]]
+    for ind, chem in enumerate(cmap_db['pubchem_id']):
+        if chem in list(cfm_db["CID"]):
+            cids.append(chem)
+            dist.append(cmap_db['cosine_dist'].loc[ind])
+    statistics = []
+    for i in range(10):
+        dist_rand = np.random.choice(list(cmap_db['cosine_dist']), len(dist))
+        stat, pval = stats.ks_2samp(dist, dist_rand)
+        statistics.append((stat, pval))
+        sns_plot = sns.displot([dist, dist_rand], kde=True)
+        sns_plot.savefig(pref+str(i)+".png")
 
-cfm_db = pd.read_csv("~/Topo_Camp/Data/intersection_of_CFM_and_L1000FWD.csv")
-cmap_db = pd.read_csv("~/PycharmProjects/Topo_CM/small_mol.csv")
-dist = []
-cids = []
-cfm_db["CID"] = [str(cid) for cid in cfm_db["CID"]]
-for ind, chem in enumerate(cmap_db['pubchem_id']):
-    if chem in list(cfm_db["CID"]):
-        cids.append(chem)
-cids = list(set(cids))
-for ind, chem in enumerate(cmap_db['pubchem_id']):
-    if chem in cids:
-        dist.append(cmap_db['cosine_dist'].loc[ind])
-        cids.remove(chem)
-print(len(dist))
-for i in range(10):
-    dist_rand = np.random.choice(list(cmap_db['cosine_dist']), len(dist))
-    stat, pval = stats.ks_2samp(dist, dist_rand)
-    print(stats.ks_2samp(dist, dist_rand))
-    sns.histplot(dist, color='pink', kde=True)
-    sns.histplot(dist_rand, kde=True)
-    plt.show()
+    return statistics
