998fc43825e8087edafa8e4f597c6e0724db4abf
petite_marie <minaeva.mav@gmail.com>
2021-02-27 17:38:11 +0300

    Minor changes

--- Cmap.py
+++ Cmap.py
@@ -6,100 +6,112 @@ from scipy.spatial import distance
 from BD_signature_parser import *
 from metric_calculator import *
 from cmap import decomposition
+import numpy as np
+from gtf_parser import *
 
 class TopoCMap:
 
-    def __init__(self, up_request, down_request, db, mode):
+    def __init__(self, up_request, down_request, db, mode, file):
         self.up_request = up_request
         self.down_request = down_request
         self.db = db
         self.mode = mode
+        self.metrics_up = centrality_metrics(file, mode="up_genes", score=0.1).metric_calculator
+        self.metrics_down = centrality_metrics(file, mode="down_genes", score=0.1).metric_calculator
         self.inf_score_up = None
         self.inf_score_down = None
 
     def space_finding(self, db_up, db_down):
 
-        if self.mode == "reverse":
-            set_1 = list(set(self.down_request + db_up))
-            set_2 = list(set(self.up_request + db_down))
+        #if self.mode == "reverse":
+
+        #set_1 = list(set(self.down_request + db_up))
+        #set_2 = list(set(self.up_request + db_down))
             ## проверять что set1 не пересекается с set2
-        else:
-            set_1 = list(set(self.down_request + db_down))
-            set_2 = list(set(self.up_request + db_up))
+        #else:
+            #set_1 = list(set(self.down_request + db_down))
+            #set_2 = list(set(self.up_request + db_up))
+
+        set_1 = dict.fromkeys(list(set(self.down_request + db_up)), [])
+        set_2 = dict.fromkeys(list(set(self.up_request + db_down)), [])
+        print(set_1)
+        print(set_2)
+        for gene in self.down_request:
+            set_1[gene].append(0)
+
+        for gene in self.up_request:
+            set_2[gene].append(0)
+
+        for gene in db_up:
+            set_1[gene].append(1)
+
+        for gene in db_down:
+            set_2[gene].append(1)
+
 
         return set_1, set_2
 
-    def influence_score(self):
+    def influence_score(self, weights):
         inf_scores_up = []
         inf_scores_down = []
-        metrics_up = centrality_metrics(args.infile, mode="up_genes", score=0.1).metric_calculator
-        metrics_down = centrality_metrics(args.infile, mode="down_genes", score=0.1).metric_calculator
-        inf_scores_up.append(metrics_up.loc[0])
-        inf_scores_up.append([(x[0] * x[1]+1) for x in zip(metrics_up.loc[2], metrics_up.loc[3])])
-        inf_scores_down.append(metrics_down.loc[0])
-        ## добавить сюда FC
-        inf_scores_down.append([(x[0] * x[1]+1) for x in zip(metrics_down.loc[2], metrics_down.loc[3])])
-
+        inf_scores_up.append(self.metrics_up.loc[0])
+        """
+        for i in range(1, 7):
+            print(np.std(list(self.metrics_up.loc[i])))
+        """
+        vector = (np.array(list(self.metrics_up.loc[1]))*weights[0] + 1) * (np.array(list(self.metrics_up.loc[2]))*weights[1] + 1) * (np.array(list(self.metrics_up.loc[3]))*weights[2] + 1) * (np.array(list(self.metrics_up.loc[4]))*weights[3] + 1) * (np.array(list(self.metrics_up.loc[5]))*weights[4] + 1)*(np.array(list(self.metrics_up.loc[6]))*weights[5] + 1) + weights[6]
+        inf_scores_up.append(list(vector))
+        inf_scores_down.append(self.metrics_down.loc[0])
+        vector = (np.array(list(self.metrics_down.loc[1]))*weights[0] + 1) * (np.array(list(self.metrics_down.loc[2]))*weights[1] + 1) * (np.array(list(self.metrics_down.loc[3]))*weights[2] + 1) * (np.array(list(self.metrics_down.loc[4]))*weights[3]  + 1) * (np.array(list(self.metrics_down.loc[5]))*weights[4] + 1)*(np.array(list(self.metrics_down.loc[6]))*weights[5] + 1) + weights[6]
+        inf_scores_down.append(list(vector))
         self.inf_score_up = inf_scores_up
         self.inf_score_down = inf_scores_down
 
-    ## добавить в граф гены не из STRING и тогда не надо будет делать current_scores
 
-    def current_scores(self, space):
+    def current_scores(self, spaces):
         ## говорящие названия и описания классов
-        inf_scores = self.inf_score_up + self.inf_score_down
+        inf_scores = [self.inf_score_up, self.inf_score_down]
+        #inf_scores = self.inf_score_up + self.inf_score_down
         output = []
-        for sp_gene in space:
+
+        for  ind, inf_score in enumerate(inf_scores):
+            output.append([])
+            for sp_gene in spaces[ind]:
+                if sp_gene in inf_score[0]:
+                    output[ind].append(inf_score[1][inf_score[0].index(sp_gene)])
+                else:
+                    output[ind].append(1.0)
+        """
+        for sp_gene in spaces:
             if sp_gene in inf_scores[0]:
                 output.append(inf_scores[1][inf_scores[0].index(sp_gene)])
-            else:
+            else :
                 output.append(1.0)
-
+        """
         return output
 
-    """@staticmethod
-    def decomposition(data):
-        vector = []
-        out_genes = []
-        out_vector = []
-        for sp_gene in data[1]:
-            if sp_gene in data[0]:
-                out_genes.append(sp_gene)
-                out_vector.append(1.0)
-            else:
-                out_genes.append(sp_gene)
-                out_vector.append(0.0)
-        vector.append(out_genes)
-        vector.append(out_vector)
-
-        return vector
-    """
     def cmap(self, db_up, db_down):
-        start1 = time.time()
+
         set_1, set_2 = self.space_finding(db_up, db_down)
-        data = [(self.up_request, set_2), (db_down, set_2), (self.down_request, set_1), (db_up, set_1)]
+        #data = [(self.up_request, set_2), (db_down, set_2), (self.down_request, set_1), (db_up, set_1)]
+        #data = [(self.up_request, space), (db_down, space), (self.down_request, space), (db_up, space)]
+        data = [set_2, set_1]
         results = decomposition(data)
-        point1 = time.time()
-        print(point1 - start1)
-        data1 = [set_1, set_2]
-        pool1 = Parallel(n_jobs=1, pre_dispatch='all')
-        results1 = pool1(delayed(self.current_scores)(dd) for dd in data1)
-        point2 = time.time()
-        print(point2 - point1)
-        print(len(results[1]), len(results1[1]))
-        cos1 = distance.cosine(results[1], results[3], results1[1])
-        cos2 = distance.cosine(results[5], results[7], results1[0])
-        end1 = time.time()
-        print(end1 - point2)
+        spaces = [set_2, set_1]
+        scores = self.current_scores(spaces)
+        cos1 = distance.cosine(results[1], results[3], scores[1])
+        cos2 = distance.cosine(results[5], results[7], scores[0])
         cosine_dist = 0.5 * (cos1 + cos2)
-        print(0.5 * (cos1 + cos2), ' ', end1 - start1)
+        print(cosine_dist)
 
         return cosine_dist
 
-    def small_molec(self, sig_names, file_meta, file_drug):
-        self.influence_score()
+    def small_molec(self, sig_names, file_meta, file_drug, weights):
+        self.influence_score(weights)
         cos_dist = []
         start = time.time()
+        #space = gtf_parser()
+        #scores = self.current_scores(space)
         for i in tqdm.tqdm(range(0, len(self.db), 2)):
             cos_dist.append(self.cmap(self.db[i], self.db[i+1]))
         end = time.time()
@@ -122,15 +134,5 @@ class TopoCMap:
         ## добавить pert_name
         return output_data
 
-
-start = time.time()
-bd_signatures, sig_names = BD_signature_parser("/Users/littlequeen/Downloads/CD_signatures_binary_42809.gmt")
-up_request, down_request = signature_extractor("~/Downloads/DE_heart_fb_deseq2_edger.txt")
-trial = TopoCMap(list(up_request), list(down_request), bd_signatures[:200], 'reverse')
-small_mol = trial.small_molec(sig_names, "~/Downloads/CD_signature_metadata.csv", "~/Downloads/Drugs_metadata.csv")
-end = time.time()
-print(end - start)
-small_mol = pd.DataFrame(small_mol)
-small_mol = small_mol.T
-small_mol.columns = ["sign_id", "cosine_dist", "pert_id", "pubchem_id"]
-small_mol.to_csv('~/Downloads/small_mol.csv', index=False)
+## отнормировать коэффициенты и посмотреть распределения коэффициентов для метрик
+## баесовская оптимизация
