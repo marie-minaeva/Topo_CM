5bfad88440c5fd447156f38fb366f3848da9d3ee
petite_marie <minaeva.mav@gmail.com>
2021-05-09 19:30:44 +0300

    Bugs fixed

--- Topo_CMap_project/Cmap.py
+++ Topo_CMap_project/Cmap.py
@@ -9,6 +9,7 @@ from metric_calculator import *
 import numpy as np
 #from gtf_parser import *
 from signature_extractor import *
+from new_signature_extractor import *
 
 
 def decomposition(data):
@@ -32,6 +33,7 @@ def decomposition(data):
     return vector
 
 def inf_score_expression(matrix, weights):
+    """
     vector = (np.array(list(matrix.loc[1])) * weights[0] + 1) * (
                 np.array(list(matrix.loc[2])) * weights[1] + 1) * (
                          np.array(list(matrix.loc[3])) * weights[2] + 1) * (
@@ -40,13 +42,22 @@ def inf_score_expression(matrix, weights):
                          np.array(list(matrix.loc[6])) * weights[5] + 1) * (
                          np.array(list(matrix.loc[7])) * weights[6] + 1) * (
                          np.array(list(matrix.loc[8])) * weights[7] + 1) + weights[8]
+    """
+    vector = (np.array(list(matrix.loc[1])) * weights[1] + 1) * (
+                     np.array(list(matrix.loc[2])) * weights[2] + 1) * (
+                     np.array(list(matrix.loc[3])) * weights[3] + 1) * (
+                     np.array(list(matrix.loc[4])) * weights[4] + 1) * (
+                     np.array(list(matrix.loc[5])) * weights[5] + 1) * (
+                     np.array(list(matrix.loc[6])) * weights[6] + 1) * (
+                     np.array(list(matrix.loc[7])) * weights[7] + 1) + weights[8]
+
     return vector
 
 
 class TopoCMap:
 
     def __init__(self, db, mode, file):
-        self.up_request, self.down_request, self.up_FC, self.down_FC = signature_extractor(file)
+        self.up_request, self.down_request, self.up_FC, self.down_FC = new_signature_extractor(file[0], file[1])
         self.db = db
         self.mode = mode
         self.metrics_up = centrality_metrics(0.1, self.up_request, self.up_FC).metric_calculator()
@@ -77,10 +88,10 @@ class TopoCMap:
             dict_2[gene].append(0)
 
         for gene in db_up:
-            dict_1[gene].append(0)
+            dict_1[gene].append(1)
 
         for gene in db_down:
-            dict_2[gene].append(0)
+            dict_2[gene].append(1)
         ord_dict_1 = {key: dict_1[key] for key in set_1}
         ord_dict_2 = {key: dict_2[key] for key in set_2}
         return ord_dict_1, ord_dict_2
@@ -88,7 +99,6 @@ class TopoCMap:
     def influence_score(self, weights):
         inf_scores_up = []
         inf_scores_down = []
-        print(self.metrics_up)
         inf_scores_up.append(self.metrics_up.loc[0])
         """
         for i in range(1, 7):
@@ -118,8 +128,6 @@ class TopoCMap:
         data = [self.inf_score_up, self.inf_score_down]
         for ind in range(len(spaces)):
             scores.append(self.current_scores(spaces[ind], data[ind]))
-        print(scores[1])
-        print(results[3])
         cos1 = distance.cosine(results[2], results[3], np.nan_to_num(scores[1], nan=1.0))
         cos2 = distance.cosine(results[0], results[1], np.nan_to_num(scores[0], nan=1.0))
         cosine_dist = 0.5 * (cos1 + cos2)
@@ -161,3 +169,4 @@ class TopoCMap:
 
 ## отнормировать коэффициенты и посмотреть распределения коэффициентов для метрик
 ## баесовская оптимизация
+
