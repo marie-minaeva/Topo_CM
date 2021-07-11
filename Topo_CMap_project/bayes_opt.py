8a8e1868f18eb1a2f02b48730f0d03f1e3e9b8cf
marie-minaeva <61414105+marie-minaeva@users.noreply.github.com>
2021-07-11 19:33:00 +0300

    Minor changes

--- Topo_CMap_project/bayes_opt.py
+++ Topo_CMap_project/bayes_opt.py
@@ -1,14 +1,6 @@
 from skopt import gp_minimize
 from validation import validate
 
-res = gp_minimize(validate, [(1.0, 10.0), (1.0, 10.0), (1.0, 10.0), (1.0, 10.0), (1.0, 10.0), (1.0, 10.0), (1.0, 10.0), (1.0, 10.0), (0.0, 1.0)],
+res = gp_minimize(validate, [(1.0, 10.0), (1.0, 10.0), (1.0, 10.0), (1.0, 10.0), (1.0, 10.0), (1.0, 10.0), (1.0, 10.0),
+                             (1.0, 10.0), (0.0, 1.0)],
                   n_initial_points=15, n_calls=30, verbose=True)
-print(str(res))
-with open("results.txt", "w") as file:
-    file.write(res)
-
-
-
-
-
-
